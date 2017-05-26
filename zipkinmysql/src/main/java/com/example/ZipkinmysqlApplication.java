1df3af2c1e4c47a808071ee929382c460978b568
Lukas Steigerwald <lukas.steigerwald@tum.de>
2017-05-26 14:47:26 +0200

    Writing something to kafka - however not human readable

--- zipkinmysql/src/main/java/com/example/ZipkinmysqlApplication.java
+++ zipkinmysql/src/main/java/com/example/ZipkinmysqlApplication.java
@@ -3,11 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
 import zipkin.server.*;
 
 @RefreshScope
 @SpringBootApplication
-@EnableZipkinServer
+@EnableZipkinStreamServer
 public class ZipkinmysqlApplication {
 
 	public static void main(String[] args) {
