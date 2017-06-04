3d9706a3bcd23bb75fdffa4f51e972109d4ab4d8
root <root@vmmatthes2.informatik.tu-muenchen.de>
2017-06-04 15:11:29 +0200

    update

--- config-infra-service/src/main/java/com/sebis/infra/ConfigurationServiceApplication.java
+++ config-infra-service/src/main/java/com/sebis/infra/ConfigurationServiceApplication.java
@@ -3,7 +3,9 @@ package com.sebis.infra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigurationServiceApplication {
--- zipkinmysql/src/main/java/com/example/ZipkinmysqlApplication.java
+++ zipkinmysql/src/main/java/com/example/ZipkinmysqlApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import zipkin.server.*;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @RefreshScope
 @SpringBootApplication
 @EnableZipkinServer
